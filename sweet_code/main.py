f7752fb8a4ac56b1c97cecc93e245fd32de2ca0a
btviet <vietshay@gmail.com>
2024-11-13 12:46:05 +0100

    update main

--- sweet_code/main.py
+++ sweet_code/main.py
@@ -1,39 +1,57 @@
-from detect_sw_events import detect_edac_events
+from detrend_edac import detrend
 from plot_functions import (
+    create_event_plots,
     create_fd_plots,
+    create_msl_rad_dates_sep_plots,
     create_sep_plots,
     create_stormy_plots,
-    plot_real_eruption_dates,
-    plot_real_sep_onsets,
     plot_stormy_days_bin,
+    plot_sweet_events_binned,
 )
 from processing_edac import process_raw_edac
-from standardize_edac import standardize
-from validate_cme_events import validate_cme_eruptions
 from validate_sep_events import validate_sep_onsets
+from validate_sweet_events import cross_check_sweet
 
 
 def sweet():
-    process_raw_edac()
-    standardize()
-    detect_edac_events()
+    process_raw_edac()  # Reads raw EDAC, zerosets, resamples
+    detrend()
+    # detect_sweet_events() # Find SWEET SEP events and Fds
 
 
-def sweet_events_plots():
-    create_stormy_plots()
-    create_sep_plots()
-    create_fd_plots()
-    plot_stormy_days_bin()
+def rolling_method():
+    # calculate_rolling_window_rate(5)
+    # calculate_rolling_window_rate(11)
+    detrend()
+
+
+def plots():
+    create_stormy_plots()  # Create plots of all SWEET stormy days
+    create_event_plots()  # Create plots of SWEET SEP and FD events
+    create_sep_plots()  # Create plots of SWEET SEP events
+    create_fd_plots()  # Create plots of all SWEET FD events
+    plot_stormy_days_bin()  # Plot number of SWEET stormy days in bins
+    plot_sweet_events_binned()  # Plot number of SWEET event sin bins
+    create_msl_rad_dates_sep_plots()  # Plot MSL/RAD SEP events
+    # Plot all events in literature database
 
 
 def validate_real_events():
-    validate_cme_eruptions()
-    validate_sep_onsets
-    plot_real_eruption_dates()
-    plot_real_sep_onsets()
+    # validate_cme_eruptions()
+    validate_sep_onsets()
+    # plot_real_eruption_dates()
+    # plot_real_sep_onsets()
+
+
+def validate_sweet_events():
+    cross_check_sweet()
 
 
 if __name__ == "__main__":
+    # rolling_method()
     sweet()
-    sweet_events_plots()
-    validate_real_events()
+    # detrend()
+    # detect_sweet_events() # Find SWEET SEP events and Fds
+    # plots()
+    # validate_real_events()
+    # find_sep()
